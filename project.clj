3496f9645a25d95fceccae73181a073626e90f68
Fred Concklin <fred@fredri.cc>
2011-03-09 23:29:21 -0500

    added swank-clojure to dev deps

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject leijush "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
+
