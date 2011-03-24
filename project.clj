e78c9e88d0475202c57641a58e519e0bee1873fc
Fred Concklin <fred@fredri.cc>
2011-03-24 04:34:27 -0400

    added devnotes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-;; required reading: https://github.com/technomancy/leiningen/commit/270cf1dec750d7a0ea6303651cb49e027c2c2af2
+
 (defproject leijush "1.0.0-SNAPSHOT"
   :description "Clojush with lein"
   :url "https://github.com/fconcklin/leijush"
@@ -14,9 +14,13 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :min-lein-version "1.3.0"
+  :min-lein-version "1.5.0"
   :main leijush.core
-  :aot [leijush.core])			; fix this? 
+  ;; :aot [leijush.core]
+  ;; :keep-non-project-classes true
+  )
+  
+  ; :aot [leijush.core])			; fix this? 
 
 
-;; build in autodoc like here: https://github.com/weavejester/compojure/blob/master/project.clj
+
--- src/leijush/core.clj
+++ src/leijush/core.clj
@@ -1,30 +1,12 @@
-;; core.clj
-;;
-;; This file implements a version of the Push programming language and the PushGP genetic
-;; programming system in the Clojure programming language. See the accompanying README
-;; file for usage instructions and other notes.
-;;
-;; Copyright (c) 2010 Lee Spector (lspector@hampshire.edu)
-;;
-;; This program is free software: you can redistribute it and/or modify it under
-;; the terms of version 3 of the GNU General Public License as published by the
-;; Free Software Foundation, available from http://www.gnu.org/licenses/gpl.txt.
-;;
-;; This program is distributed in the hope that it will be useful, but WITHOUT ANY
-;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FxOR A
-;; PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/licenses/)
-;; for more details.
-
 (ns leijush.core
- ;; ^{:doc "This file implements a version of the Push programming language and the PushGP genetic programming system in the clojure programming language."
- ;;   :author "Clojush written by Lee Spector. Leijush maintained by Fred Concklin"}
+  (:gen-class)
   (:require [clojure.zip :as zip]
 	    [clojure.contrib.math :as math]
 	    [clojure.contrib.seq-utils :as seq-utils]
 	    [clojure.walk :as walk]
 	    [clojure.contrib.string :as string])
-  (:gen-class))
-;  )
+  ) 
+
 
 (defn bt []
   "backtrace abbreviation, to ease debugging"
