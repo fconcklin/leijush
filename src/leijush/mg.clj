7b277e6f01a33fd2742562e2aeb488509fd19e99
Fred Concklin <fred@fredri.cc>
2011-03-27 01:34:20 -0400

    got working game up

--- src/leijush/mg.clj
+++ src/leijush/mg.clj
@@ -1,60 +1,11 @@
 (ns leijush.mg
   (:use [leijush.core] [clojure.contrib.math])) ; took out this line as require
 
-;; (define-registered in 
-;;   (fn [state] (push-item (stack-ref :auxiliary 0 state) :integer state)))
-
-;; http://techbehindtech.com/2010/06/25/parsing-xml-in-clojure/
-;; http://stackoverflow.com/questions/4641964/how-to-use-update-in-in-clojure
-;; above is update-in
-;; http://java.ociweb.com/mark/clojure/article.html
-;; martin kaiser - efficiently representing populations in genetic programming 
-;; todo
-;;; build in changing capacity [just mod total number of rounds or tmi?]
-;;; build game around it
-
+(def *capacity-list* (filter odd? (range 20)))
 (def *popsize* 10)
-(def *roundsnum* 5)
+(def *rounds-num* 5)
 
 (defstruct player :number :choices :payoffs)
-
-(defn get-decisions
-  "returns a list of all player decisions for the past round"
-  [playerlist]
-  (map #(last (% :choices)) playerlist))
-
-(defn get-all-decisions
-  "returns a list of all decisions for all players"
-  [playerlist]
-  (map #(% :choices) playerlist))
-
-(defn get-player-decisions
-  "returns a list of player's past decisions"
-  [playernum playerlist]
-  (nth (get-all-decisions playerlist) playernum))
-
-(defn payoff-sum
-  "sum the player decisions with proper weights"
-  [decisions]
-  (apply + decisions))			; integrate weights 
-
-(defn apply-payoff			
-  "add the payoff to each player"
-  [payoff player-struct]
-  (cond					; clean this up from cond
-   (= (last (player-struct :choices)) 0) (update-in player-struct [:payoffs] conj 1) ; fix usage of static value, this is for testing
-   (= (last (player-struct :choices)) 1) (update-in player-struct [:payoffs] conj payoff)))
-
-(defn calculate-payoff
-  "returns list of players with payoff applied"
-  [playerlist]
-  (let [payoff (payoff-sum (get-decisions playerlist))]
-    (map #(apply-payoff payoff %) playerlist)))
-
-(defn create-players [popsize]
-  "create the initial struct of players"
-  (for [x (range 0 (inc popsize))]
-	(struct-map player :number x :choices [] :payoffs [])))
 
 ;;; this is where player logic is inserted
 ;;; player decisions is a list of the player's past decisions
@@ -62,6 +13,45 @@
 (defn player-logic [player-decisions all-decisions] 
   "random player logic"
   (rand-int 2))
+
+(defn create-players [popsize]
+  "create the initial struct of players with empty keys"
+  (for [x (range 0 (inc popsize))]
+	(struct-map player :number x :choices [] :payoffs [])))
+
+(defn get-decisions
+  "returns a list of all player decisions for the past round"
+  [playerlist]
+  (map #(last (% :choices)) playerlist))
+
+(defn get-all-decisions
+  "returns a list of all decisions for all players (before current round)"
+  [playerlist]
+  (map #(% :choices) playerlist))
+
+(defn get-player-decisions
+  "returns a list of individual player's past decisions"
+  [playernum playerlist]
+  (nth (get-all-decisions playerlist) playernum))
+
+(defn payoff-sum
+  "sum the player decisions with proper weights"
+  [decisions capacity]
+  (* capacity
+     (apply + decisions)))			; integrate other weights 
+
+(defn apply-payoff			
+  "add the payoff to each player"
+  [payoff player-struct]
+  (if (= (last (player-struct :choices)) 0)
+    (update-in player-struct [:payoffs] conj 1)
+    (update-in player-struct [:payoffs] conj payoff)))
+
+(defn calculate-payoff
+  "returns list of players with payoff applied"
+  [playerlist capacity]
+  (let [payoff (payoff-sum (get-decisions playerlist) capacity)]
+    (map #(apply-payoff payoff %) playerlist)))
 
 (defn player-decide
   "player decide working"
@@ -71,19 +61,35 @@
 
 (defn play-rounds
   "function to play rounds. returns list of player structs"
-  [roundnum]
+  [roundnum capacity]
   (cond
    (zero? roundnum) (create-players 8)
-   :else (-> (dec roundnum)
-	     (play-rounds)
-	     (player-decide)
-	     (calculate-payoff))))
+   :else (calculate-payoff
+	  (player-decide
+	   (play-rounds (dec roundnum) capacity))
+	  capacity)))
 
-(defn rounds
-  "returns list of players with payoffs and choices for rounds"
-  [roundnum]
-  (play-round roundnum))
-  
+(defn game
+  "returns list of players with payoffs and choices in list of rounds"
+  []
+  (for [x *capacity-list*]
+    (play-rounds *rounds-num* x)))
+
+;; (defn game
+;;   "returns list of players with payoffs and choices for rounds"
+;;   [roundnum]
+;;   (loop [result [] x *capacity-list*]
+;;     (if (zero? x)
+;;       result
+;;       (recur (conj result (play-rounds 7)) (dec x)))))
+
+;;;;;;;;;;
+;; push ;;
+;;;;;;;;;;
+
+;; (define-registered in 
+;;   (fn [state] (push-item (stack-ref :auxiliary 0 state) :integer state)))
+
 ;; (pushgp 
 ;;   :error-function (fn [program]
 ;;                     (doall
@@ -120,3 +126,12 @@
 ;;     (get-decisions
 ;;      (play-round roundnum)))
 ;;    playerlist))
+
+;; http://techbehindtech.com/2010/06/25/parsing-xml-in-clojure/
+;; http://stackoverflow.com/questions/4641964/how-to-use-update-in-in-clojure
+;; above is update-in
+;; http://java.ociweb.com/mark/clojure/article.html
+;; martin kaiser - efficiently representing populations in genetic programming 
+;; todo
+;;; build in changing capacity [just mod total number of rounds or tmi?]
+;;; build game around it
