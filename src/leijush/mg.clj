d226d23b4d2e2ded99d4b47834f3a327e451d809
Fred Concklin <fred@fredri.cc>
2011-03-27 03:50:23 -0400

    working on importing push

--- src/leijush/mg.clj
+++ src/leijush/mg.clj
@@ -1,23 +1,26 @@
 (ns leijush.mg
-  (:use [leijush.core] [clojure.contrib.math])) ; took out this line as require
+  (:use [leijush.core] [clojure.contrib.math]))
 
 (def *capacity-list* (filter odd? (range 20)))
 (def *popsize* 10)
 (def *rounds-num* 5)
 
-(defstruct player :number :choices :payoffs)
+(defstruct player :number :choices :payoffs :capacity)
 
 ;;; this is where player logic is inserted
 ;;; player decisions is a list of the player's past decisions
 ;;; all-decisions is a list of all past decisions, including the player
-(defn player-logic [player-decisions all-decisions] 
+(defn player-logic [player-decisions all-decisions] ; this is where push goes
   "random player logic"
-  (rand-int 2))
+  (rand-int 2)
+  ;; push program
+  ;; registered instructions for throwing player-decisions and all-decisions onto stack
+  )
 
-(defn create-players [popsize]
+(defn create-players [popsize capacity]
   "create the initial struct of players with empty keys"
   (for [x (range 0 (inc popsize))]
-	(struct-map player :number x :choices [] :payoffs [])))
+	(struct-map player :number x :choices [] :payoffs [] :capacity capacity)))
 
 (defn get-decisions
   "returns a list of all player decisions for the past round"
@@ -37,7 +40,7 @@
 (defn payoff-sum
   "sum the player decisions with proper weights"
   [decisions capacity]
-  (* capacity
+  (+ capacity
      (apply + decisions)))			; integrate other weights 
 
 (defn apply-payoff			
@@ -63,7 +66,7 @@
   "function to play rounds. returns list of player structs"
   [roundnum capacity]
   (cond
-   (zero? roundnum) (create-players 8)
+   (zero? roundnum) (create-players *popsize* capacity)
    :else (calculate-payoff
 	  (player-decide
 	   (play-rounds (dec roundnum) capacity))
@@ -72,16 +75,54 @@
 (defn game
   "returns list of players with payoffs and choices in list of rounds"
   []
-  (for [x *capacity-list*]
-    (play-rounds *rounds-num* x)))
+  (vec (flatten
+   (for [x *capacity-list*]
+    (play-rounds *rounds-num* x)))))
 
-;; (defn game
-;;   "returns list of players with payoffs and choices for rounds"
-;;   [roundnum]
-;;   (loop [result [] x *capacity-list*]
-;;     (if (zero? x)
-;;       result
-;;       (recur (conj result (play-rounds 7)) (dec x)))))
+(defn scores-map
+  "return this players with their payoff scores for game"
+  []
+  (let [data (game)]
+    (for [x (range *popsize*)]
+      (apply + (map #(apply + (:payoffs %)) (filter #(= (:number %) x) data))))))
+
+(defn winner-map
+  "returns the winners of the game sorted by payoffs. Key is player number, value is payoff total"
+  []
+  (sort-by last >
+	   (let [data (game)]
+	     (for [x (range *popsize*)]
+	       [(keyword (str x)) (apply + (map #(apply + (:payoffs %)) (filter #(= (:number %) x) data)))]))))
+
+(define-registered out
+  (fn [state]
+    (let [astate (stack-ref :auxiliary 0 state)]
+      (->> state
+	   (pop-item :auxiliary)
+	   (push-item 0 :auxiliary)))))
+
+(defin-registered in
+  (fn [state]
+    (let [astate (stack-ref :auxiliary 0 state)]
+      (->> state
+	   (pop-item :auxiliary)
+	   (push-item 0 :auxiliary)))))
+
+(defn gp-start
+  []
+  (pushgp
+   :error-function (fn [program]
+		     (map #(- 3000 %) (scores-map))))
+  :atom-generators (concat
+		    (registered-for-type :integer)
+		    (registered-for-type :exec)
+		    (registered-for-type :boolean)
+		    (list
+		     'boolean_and
+		     'boolean_not
+		     'boolean_or))
+  :error-threshold 40
+  :reproduction-simplifications 10)
 
 ;;;;;;;;;;
 ;; push ;;
@@ -111,21 +152,6 @@
 ;; 	 :population-size 5000
 ;; 	 :trivial-geography-radius 10)
 
-;; (defn game
-;;   "returns player values for a game"
-;;   []
-;;   (for [a (range 1 rounds)]
-;;     )
-;;   )
-
-;; (defn round
-;;   "returns list of players with payoffs and choices for up to round"
-;;   [playerlist roundnum]
-;;   (put-payoff
-;;    (calculate-payoff
-;;     (get-decisions
-;;      (play-round roundnum)))
-;;    playerlist))
 
 ;; http://techbehindtech.com/2010/06/25/parsing-xml-in-clojure/
 ;; http://stackoverflow.com/questions/4641964/how-to-use-update-in-in-clojure
