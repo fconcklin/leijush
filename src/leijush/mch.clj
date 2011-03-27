19a3e039f2beb681b72b293c5bfe04c4ba2ece11
Fred Concklin <fred@fredri.cc>
2011-03-27 00:01:41 -0400

    working on game

--- src/leijush/mch.clj
+++ src/leijush/mch.clj
@@ -44,23 +44,17 @@
 (defn runRound
   ; makes a round by cons-ing runChoos recusively
   [tournament players cap]
-  (let [z (genetic-runner (first players) (second tournament) (first (first
-								      tournament)) cap)
-        x
-        (if (= z 0) 0 1)]
-    (cons
-     x
+  (let [z (genetic-runner (first players) (second tournament) (first (first tournament)) cap)
+        x (if (= z 0) 0 1)]
+    (cons x
      (pmap
       (fn [x y]
-	(runChoose
-	 x
-	 (second tournament)
-	 y
-	 cap))
+	(runChoose x(second tournament) y cap))
       (rest players)
       (rest (first tournament))))))
 
 (defn listMaker
+  "makes a list"
   [size]
   (loop [lisT (list)]
     (if (= (count lisT) (inc size))
@@ -68,21 +62,21 @@
       (recur (cons (list) lisT)))))
 
 (defn runGame
-  ; makes a game by cons-ing runRound recursively
+  "makes a game by cons-ing runRound recursively"
   [tournament gamePertournament players]
   (loop [game (list (listMaker (count players)) (list))
          counter 0]
     (if (= gamePertournament counter)
       (list
-        (pmap cons (first game) (first tournament))
-        (cons (second game) (second tournament)))
+       (pmap cons (first game) (first tournament))
+       (cons (second game) (second tournament)))
       (let [acts (runRound tournament players (inc (* 2 counter)))
             pay (payOff acts (inc (* 2 counter)))]
         (recur
-          (list
-            (pmap cons pay (first game))
+	 (list
+	  (pmap cons pay (first game))
           (cons acts (second game)))
-          (inc counter))))))
+	 (inc counter))))))
 
 (defn runTourn
   [numOftournament gamePertournament players]
@@ -93,41 +87,40 @@
       (recur (runGame tournament gamePertournament players) (inc counter)))))
 
 (defn strategy1
-  ; A sample strategy
+  "A sample strategy"
   [touny history cap]
   (rand-int 2))
 
 (defn strategy2
-  ; A sample strategy
+  "A sample strategy"
   [touny history cap]
   (if (< (inc (rand-int 20)) cap)
     1
     0))
 
-(defn getScore
-  [payOffs]
+(defn getScore [payOffs]
   (pmap
-    (fn [x]
-        (apply + x))
-    payOffs))
+   (fn [x]
+     (apply + x))
+   payOffs))
 
-(defn run
+(defn run				; error-threshold 40
   [error strat]
   (pushgp
-    :error-function (fn [program]
-                      (doall
-		       (map
-			(fn [x]
-			  (- 50 x))
-			(getScore
-			 (last
-			  (first
-			   (runTourn 10 10
-				     (cons program
-					   (loop [players (list)]
-					     (if (= (count players) 19)
-					       players
-					       (recur (cons strat players))))))))))))
+   :error-function (fn [program]
+		     (doall
+		      (map
+		       (fn [x]
+			 (- 50 x))
+		       (getScore
+			(last
+			 (first
+			  (runTourn 10 10
+				    (cons program
+					  (loop [players (list)]
+					    (if (= (count players) 19)
+					      players
+					      (recur (cons strat players))))))))))))
     :atom-generators (concat
                        (registered-for-type :integer)
                        (registered-for-type :exec)
@@ -163,18 +156,21 @@
           players
           (recur (cons strat players)))))))
 
-(run 40 strategy2)
+(run 40 strategy2)			; error-threshold
+
+
 
 ;'(28 20 16 16 20 22 18 8 22 10)
 ;(getScore (first (first (playgame '(code_if integer_sub (exec_eq integer_dup
-(integer_min)) (boolean_stackdepth boolean_yank (boolean_pop integer_eq)
-(boolean_rand)) (exec_noop ((integer_rand) boolean_shove (exec_eq exec_s)
-integer_mod) (boolean_and (integer_div integer_pop (integer_div) (exec_swap))
-(boolean_and exec_stackdepth (integer_dup))) boolean_or boolean_shove)
-integer_eq exec_stackdepth) strategy2))))
-(first (second (playgame '(code_if integer_sub (exec_eq integer_dup
-(integer_min)) (boolean_stackdepth boolean_yank (boolean_pop integer_eq)
-(boolean_rand)) (exec_noop ((integer_rand) boolean_shove (exec_eq exec_s)
-integer_mod) (boolean_and (integer_div integer_pop (integer_div) (exec_swap))
-(boolean_and exec_stackdepth (integer_dup))) boolean_or boolean_shove)
-integer_eq exec_stackdepth) strategy2)))
+
+;; (integer_min)) (boolean_stackdepth boolean_yank (boolean_pop integer_eq)
+;; (boolean_rand)) (exec_noop ((integer_rand) boolean_shove (exec_eq exec_s)
+;; integer_mod) (boolean_and (integer_div integer_pop (integer_div) (exec_swap))
+;; (boolean_and exec_stackdepth (integer_dup))) boolean_or boolean_shove)
+;; integer_eq exec_stackdepth) strategy2))))
+;; (first (second (playgame '(code_if integer_sub (exec_eq integer_dup
+;; (integer_min)) (boolean_stackdepth boolean_yank (boolean_pop integer_eq)
+;; (boolean_rand)) (exec_noop ((integer_rand) boolean_shove (exec_eq exec_s)
+;; integer_mod) (boolean_and (integer_div integer_pop (integer_div) (exec_swap))
+;; (boolean_and exec_stackdepth (integer_dup))) boolean_or boolean_shove)
+;; integer_eq exec_stackdepth) strategy2)))
--- src/leijush/mg.clj
+++ src/leijush/mg.clj
@@ -2,8 +2,8 @@
   (:require [leijush.core] [clojure.contrib.math])
   (:use [leijush.core] [clojure.contrib.math]))
 
-(define-registered in 
-  (fn [state] (push-item (stack-ref :auxiliary 0 state) :integer state)))
+;; (define-registered in 
+;;   (fn [state] (push-item (stack-ref :auxiliary 0 state) :integer state)))
 
 ;; http://techbehindtech.com/2010/06/25/parsing-xml-in-clojure/
 ;; http://stackoverflow.com/questions/4641964/how-to-use-update-in-in-clojure
@@ -21,92 +21,101 @@
 
 (defstruct player :number :choices :payoffs)
 
+(defn get-decisions
+  "returns a list of all player decisions for the past round"
+  [playerlist]
+  (map #(last (% :choices)) playerlist))
+
+(defn get-all-decisions
+  "returns a list of all decisions for all players"
+  [playerlist]
+  (map #(% :choices) playerlist))
+
+(defn get-player-decisions
+  "returns a list of player's past decisions"
+  [playernum playerlist]
+  (nth (get-all-decisions playerlist) playernum))
+
+(defn payoff-sum
+  "sum the player decisions with proper weights"
+  [decisions]
+  (apply + decisions)			; integrate weights 
+  )
+
+(defn apply-payoff			
+  "add the payoff to each player"
+  [payoff player-struct]
+  (cond					; clean this up from cond
+   (= (last (player-struct :choices)) 0) (update-in player-struct [:payoffs] conj 1) ; fix usage of static value, this is for testing
+   (= (last (player-struct :choices)) 1) (update-in player-struct [:payoffs] conj payoff)))
+
+(defn calculate-payoff
+  "returns list of players with payoff applied"
+  [playerlist]
+  (let [payoff (payoff-sum (get-decisions playerlist))]
+    (map #(apply-payoff payoff %) playerlist)))
+
+(defn create-players [popsize]
+  "create the initial struct of players"
+  (for [x (range 0 (inc popsize))]
+	(struct-map player :number x :choices [] :payoffs [])))
+
+;; has access to
+;;; past-decisions
+(defn player-logic []			; this is where push players will go 
+  "random player logic"
+  (rand-int 2))
+
+;; (defn player-decide2
+;;   "returns players with decision in :choices key"
+;;  [player-structs]
+;;  (let [past-decisions (get-all-decisions player-structs)]
+;;      (for [x (range 1 (count player-structs))]
+;;        (let [player-decisions (get-player-decisions x past-decisions)]
+;; 	 (update-in (nth ps x) [:choices] conj (player-logic)))))
+
+(defn player-decide
+  "player decide working"
+  [player-structs]
+  (map #(update-in % [:choices] conj (player-logic)) player-structs))
+  
+
+(defn play-rounds
+  "function to play rounds. returns list of player structs"
+  [roundnum]
+  (cond
+   (zero? roundnum) (create-players 8)
+   :else (-> (dec roundnum)
+	     (play-rounds)
+	     (player-decide)
+	     (calculate-payoff))))
+
 (defn rounds
   "returns list of players with payoffs and choices for rounds"
   [roundnum]
    (calculate-payoff			; move into (play-round)
     (play-round roundnum)))
-
-; old 					;  (let [payoff (payoff-sum (apply + (map last playerlist)))]
-(defn calculate-payoff
-  "returns list of players with payoff applied"
-  [playerlist]
-  (let [payoff (payoff-sum (get-decisions playerlist))]
-    (map #(apply-payoff payoff %) playerlist))) ;; update-in 
-
-;;; for each struct, give it a payoff based on whether or not it entered in last round
-;;; use merge-with a keyword
-;;; figure out how to merge back in / maybe atom instead?
-(defn apply-payoff			
-  "add the payoff to each player"
-  [payoff player-struct]
-  nil
-  )
-
-(defn payoff-sum
-  "sum the player decisions with proper weights"
-  [decisions]				; takes list of decisions
-  nil
-  )
-
-(defn get-decisions
-  "returns a list of all player decisions for the past round"
-  [playerlist]
-  nil
-  )
-
-;;; this is a recursive function that plays out the rounds
-(defn play-round
-  "function to play a round. returns list of player structs"
-  [roundnum]
-  ;; should I move calculate payment down here?
-   (if (zero? roundnum)
-     (create-players 8)			; put in popsize
-     (map #(player-decide %) (play-round (- roundnum 1))))) ; put calculate-payoff here
-
-(defn player-logic []
-  "random player logic"
-  (rand-int 2))
-
-;; this is also where push players will go
-(defn create-players [popsize]
-  "create the initial struct of players"
-  (for [x (range 1 popsize]
-	(struct-map player :number x))))
-
-;; this is where we'll be pulling off int or bool stackfrom push
-(defn player-decide [player-struct]
-  "returns player with decision in :choices key"
-  ;; assoc player logic with choices keyword
-  (player-logic))			; stack throw
-
-;; (for [x (range 0 popsize)] ;; declist
-;;   (player-decide x))
-
-;; (merge-with cons
-;; 	    (:choices plr-list)
-;; 	    declist)
   
-(pushgp 
-  :error-function (fn [program]
-                    (doall
-                      (for [input (range 1 6)]
-                        (let [state (run-push program
-                                      (push-item input :auxiliary
-                                        (push-item input :integer
-                                          (make-push-state))))
-                              top-int (top-item :integer state)]
-                          (if (number? top-int)
-                            (abs (- top-int (factorial input)))
-                            1000000000))))) ;; big penalty, since errors can be big
-	 :atom-generators (concat (registered-for-type :integer)
-                     (registered-for-type :exec)
-                     (registered-for-type :boolean)
-                     (list (fn [] (rand-int 100))
-                       'in))
-	 :max-points 100
-	 :population-size 5000
-	 :trivial-geography-radius 10)
+;; (pushgp 
+;;   :error-function (fn [program]
+;;                     (doall
+;;                       (for [input (range 1 6)]
+;;                         (let [state (run-push program
+;;                                       (push-item input :auxiliary
+;;                                         (push-item input :integer
+;;                                           (make-push-state))))
+;;                               top-int (top-item :integer state)]
+;;                           (if (number? top-int)
+;;                             (abs (- top-int (factorial input)))
+;;                             1000000000))))) ;; big penalty, since errors can be big
+;; 	 :atom-generators (concat (registered-for-type :integer)
+;;                      (registered-for-type :exec)
+;;                      (registered-for-type :boolean)
+;;                      (list (fn [] (rand-int 100))
+;;                        'in))
+;; 	 :max-points 100
+;; 	 :population-size 5000
+;; 	 :trivial-geography-radius 10)
 
 ;; (defn game
 ;;   "returns player values for a game"
